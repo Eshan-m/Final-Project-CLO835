658a86f6dc074b09eec8bc16b32758b609824793
Eshan-m <thineemadura@gmail.com>
2024-11-19 16:47:56 +0000

    port changes

--- app.py
+++ app.py
@@ -78,6 +78,6 @@ def about():
 if __name__ == "__main__":
     try:
         logging.info("Starting Flask application...")
-        app.run(host="0.0.0.0", port=81)
+        app.run(host="0.0.0.0", port=8080)
     except Exception as e:
         logging.error(f"Error running the Flask application: {e}")
